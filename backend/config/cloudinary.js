148f5f7bb937651a164b845ffe25f3952ab36171
pragyanverma39 <pragyanverma.9871@gmail.com>
2026-06-19 17:27:30 +0530

    Update Cloudinary configuration and security improvements

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: "dri0wbrtu",
-  api_key: "329722235357286",
-  api_secret: "emHvoK_NVCMSFl0fI5gtrdARoTk",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 module.exports = cloudinary;
